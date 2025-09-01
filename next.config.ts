1318e42acdd4c70f96240625aab6dbf3045e218b
wheon06 <up2kid@gmail.com>
2025-09-01 15:05:07 +0900

    feat: Github Page 배포

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const repoName = 'oreurock-home';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export',
+    images: { unoptimized: true },
+    basePath: `/${repoName}`,
+    assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
